6e579ab9034ae64cf7f4bff3575bb2e5ccefe0a2
Chris Kirby <chris.kirby@asurion.com>
2020-09-01 05:56:21 -0400

    Setup postgres container.

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# dev db - localhost flask app; localhost sqlite db
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database', 'app.db')
-# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'database', 'migrations')
-OTP_SECRET = 'abc'
+
+# test db - localhost flask app; db running in container
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://airflow:airflow@localhost:5432'
+
+# prod db - flask app and db running in containers
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://airflow:airflow@db:5432'
+
+OTP_SECRET = 'Wdj5ccVm4pGbDy'
 DEBUG = True
 
-UPLOAD_FOLDER = '/Users/ckirby/PycharmProjects/tv_failure_recognition/app/static'
+
+UPLOAD_FOLDER = '/Users/ckirby/git/calypso/app/static'
 ALLOWED_EXTENSIONS = {'txt', 'csv'}
