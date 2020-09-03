d31111a0852fe60fc7537535e84faaf4a1ad0384
Chris Kirby <chris.kirby@asurion.com>
2020-09-03 06:50:49 -0400

    Refactored behave setup.

--- features/environment.py
+++ features/environment.py
@@ -1,13 +1,17 @@
 from behave import fixture, use_fixture
 from selenium import webdriver
 
+CHROMEDRIVER_PATH = '/Users/ckirby/git/calypso/features/chromedriver'
+
 @fixture
 def get_browser(context):
-    context.browser = webdriver.Chrome('/Users/ckirby/git/calypso/features/chromedriver')
+    """Sets up Chrome browser for testing."""
+    context.browser = webdriver.Chrome(CHROMEDRIVER_PATH)
     yield context.browser
     context.browser.quit()
     context.browser = None
 
 def before_all(context):
+    """Load browser befor starting tests"""
     use_fixture(get_browser, context)
 
--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,9 +1,8 @@
 import pathlib
 import sys
-"""
-This is a hack to load the flask app into behave's context
-"""
-# grab the current file and go up to levels. This is the app/repo's root.
+
+# This is a hack to load the flask app into behave's context
+# Grab the current file and go up two levels. This is the app/repo's root.
 app_root_dir = str(pathlib.Path(__file__).parents[2])
 # insert into python sys path so app module is importable.
 sys.path.insert(0, app_root_dir)
@@ -15,6 +14,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+"""
+Model helpers
+"""
 @given('these uploaded files exist')
 def step_impl(context):
     for row in context.table:
@@ -22,6 +25,16 @@ def step_impl(context):
         if not models.UploadFile.find_by_name(row['file_name']):
             models.UploadFile(row['file_name']).save()
 
+@given(u'"{file_name}" exists')
+def step_impl(context, file_name):
+    upload_file = models.UploadFile.find_by_name(file_name)
+    assert_that(upload_file, instance_of(models.UploadFile))
+    assert_that(upload_file.file_name, equal_to(file_name))
+
+
+"""
+Navigation helpers
+"""
 @step('a user navigates to home page')
 def step_impl(context):
     context.browser.get('http://localhost:5001/index')
@@ -42,28 +55,26 @@ def step_impl(context, name):
     ele = context.browser.find_element_by_xpath(f'//span[contains(text(), "{name}")]')
     ele.click()
 
+@when(u'user selects "example.csv" in file browser')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When user selects "example.csv" in file browser')
+
+
+"""
+Validation helpers
+"""
 @then(u'user should see "{text}"')
 def step_impl(context, text):
     # wait for a few seconds for async calls to load
     ele = WebDriverWait(context.browser, 5).until(EC.visibility_of_element_located((By.XPATH, f'//*[contains(text(), "{text}")]')))
     assert_that(ele.text, equal_to(text))
 
-@when(u'user selects "example.csv" in file browser')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When user selects "example.csv" in file browser')
-
-
 @then(u'user should not see upload dialog')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Then user should not see upload dialog')
-
 
 @then(u'user should see download dialog')
 def step_impl(context):
     raise NotImplementedError(u'STEP: Then user should see download dialog')
 
-@given(u'"{file_name}" exists')
-def step_impl(context, file_name):
-    upload_file = models.UploadFile.find_by_name(file_name)
-    assert_that(upload_file, instance_of(models.UploadFile))
-    assert_that(upload_file.file_name, equal_to(file_name))
+
